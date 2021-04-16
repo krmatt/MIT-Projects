535ec8b96f9e21dd0328bcd2a13cdc21dcca11df
Matt <matthew.kramer@uconn.edu>
2021-04-15 21:00:58 -0400

    Create EM.py

---
+++ hw4/EM.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def gauss_est(k, x, pi, mu, s2):
+    g1 = pi[0] * 
+    return p
+
+theta_0 = [0.5, 0.5, 6, 7, 1, 4]
+pi = [theta_0[0], theta_0[1]]
+mu = [theta_0[2], theta_0[3]]
+s2 = [theta_0[4], theta_0[5]]
+k = [1, 2]
+x = [-1, 0, 4, 5, 6]
